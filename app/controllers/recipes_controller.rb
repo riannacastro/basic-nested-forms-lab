68d9871545af39f68b68fb127871242084510368
Rianna <riannaxcastro@gmail.com>
2021-07-23 22:01:57 -1000

    commit tree c9aa93f71ef56e3d85153f26c1a54df8ca71d1c1

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,25 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(name: "Ingredient One")
+    @recipe.ingredients.build(name: "Ingredient Two")
+
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+  
+    if @recipe.save
+      redirect_to recipes_path(@recipe)
+    else
+      render :new
+    end
   end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
+  end
+  
 end
